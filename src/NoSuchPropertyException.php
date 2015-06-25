318f5889002302217c53f8fc4b302db0d2ae98e1
Corey Frenette <LordMonoxide@users.noreply.github.com>
2015-06-24 23:24:55 -0300

    Update NoSuchPropertyException.php

--- src/NoSuchPropertyException.php
+++ src/NoSuchPropertyException.php
@@ -3,5 +3,13 @@
 use Exception;
 
 class NoSuchPropertyException extends Exception {
+  private $property;
   
+  public function __construct($property, $message = null, $code = 0, Exception $previous = null) {
+    $this->property = $property;
+  }
+  
+  public function getProperty() {
+    return $this->property;
+  }
 }
